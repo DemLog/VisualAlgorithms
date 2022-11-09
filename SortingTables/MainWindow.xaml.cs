8b56f058721f18d030234148093d3f58646d59de
DemLoveSky <dem.loginov@gmail.com>
2022-11-09 07:03:29 +0500

    Release 1

---
+++ SortingTables/MainWindow.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.IO;
+
+namespace SortingTables
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DrawMove.ping = ping;
+        }
+        public List<Table> Rows;
+        private void tablesgr_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void SortClock(object sender, RoutedEventArgs e)
+        {
+            var sa = new SortingAlgorithm(content, logs);
+            sa.MergeSort(Rows.ToArray());
+            GetTable();
+        }
+        
+        public static bool checkBox = false;
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            checkBox = true;
+
+        }
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            checkBox = false;
+
+        }
+
+        private void LoadedFile(string path)
+        {
+            string[] tv = new string[0];
+            columns.Children.Clear();
+            SortColumns.Clear();
+            logs.Children.Clear();
+                
+            if (checkBox)
+                tv = File.ReadAllText(@path).Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            else
+                tv = File.ReadAllLines(@path);
+            
+            var cl = tv[0].Split(new char[] { ';' });
+
+            if (cl.Length == 1)
+            {
+                columns.Children.Add(GetBt("Текст", 0));
+            }
+            else
+            {
+                for (int i = 0; i < cl.Length; i++)
+                {
+                    columns.Children.Add(GetBt(cl[i], i));
+                }
+            }
+
+            Rows = new List<Table>();
+
+            for (var i = 1; i < tv.Length; i++)
+            {
+                var row = tv[i].Split(new char[] { ';' });
+                Rows.Add(Table.GetTable(row));
+            }
+
+            SortColumns.OrderBy(el => el);
+            GetTable();
+        }
+        private void GetTable()
+        {
+            content.Children.Clear();
+            foreach(var el in Rows)
+            {
+                content.Children.Add(Table.GetStack(el, Brushes.White));
+            }
+        }
+        private void Minus100Click(object sender, RoutedEventArgs e)
+        {
+            ping.Text = (int.Parse(ping.Text) - 100).ToString();
+        }
+
+        private void Minus10Click(object sender, RoutedEventArgs e)
+        {
+            ping.Text = (int.Parse(ping.Text) - 10).ToString();
+        }
+
+        private void Plus10Click(object sender, RoutedEventArgs e)
+        {
+            ping.Text = (int.Parse(ping.Text) + 10).ToString();
+        }
+
+        private void Plus100Click(object sender, RoutedEventArgs e)
+        {
+            ping.Text = (int.Parse(ping.Text) + 100).ToString();
+        }
+        private Button GetBt(string name, int i)
+        {
+            var bt = new Button();
+            bt.Content = name;
+            bt.Click += Bt_Click;
+            bt.Uid = i.ToString();
+
+            return bt;
+        }
+        public static List<int> SortColumns = new List<int>();
+        private void Bt_Click(object sender, RoutedEventArgs e)
+        {
+            int id = Int32.Parse(((Button)sender).Uid);
+            
+            if (SortColumns.Contains(id))
+                SortColumns.Remove(id);
+            else
+                SortColumns.Add(id);
+            
+            if(((Button)sender).Background == Brushes.Red)
+            {
+                ((Button)sender).Background = default(Brush);
+            }
+            else
+            {
+                ((Button)sender).Background = Brushes.Red;
+            }
+        }
+        private void SelectFileClick(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.Filter = "Text documents (*.txt)|*.txt|All files (*.*)|*.*";
+            dialog.FilterIndex = 2;
+
+            Nullable<bool> result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = dialog.FileName;
+                ((Button)sender).Content = Path.GetFileName(filename);
+                this.LoadedFile(filename);
+            }
+        }
+    }
+}
---
+++ SortingTables/Table.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using OfficeOpenXml;
+
+namespace SortingTables
+{
+    public class Table
+    {
+        public Table(string[] row)
+        {
+            Column = row;
+        }
+        public string[] Column { get; set; }
+        public override string ToString()
+        {
+            StringBuilder row = new StringBuilder();
+            foreach (var col in Column)
+            {
+                row.Append(col + ";");
+            }
+
+            return row.ToString();
+        }
+        public static Table GetTable(string[] array)
+        {
+            return new Table(array);
+        }
+        public static bool operator <(Table left, Table right)
+        {
+            return Sorting(right, left);
+        }
+        public static bool operator >(Table left, Table right)
+        {
+            return Sorting(left, right);
+        }
+        private static bool Sorting(Table left, Table right)
+        {
+            foreach(var el in MainWindow.SortColumns)
+            {
+                if (!GetColumn(left, el).Equals(GetColumn(right, el)))
+                {
+                    if (long.TryParse(GetColumn(left, el), out var a) && long.TryParse(GetColumn(right, el), out var b))
+                    {
+                        return a > b;
+                    }
+                    else if (double.TryParse(GetColumn(left, el).Replace('.', ','), out var c) &&
+                             double.TryParse(GetColumn(right, el).Replace('.', ','), out var d))
+                    {
+                        return c > d;
+                    }
+                    else
+                    {
+                        return CompareStrings(GetColumn(left, el), GetColumn(right, el));
+                    }
+
+                }
+            }
+
+            return false;
+        }
+        private static string GetColumn(Table tbl, int i)
+        {
+            return tbl.Column[i];
+        }
+        
+        private static bool CompareStrings(string left, string right)
+        {
+            for(int i = 0; i < Math.Min(left.Length, right.Length); i++)
+            {
+                if (left[i] > right[i])
+                    return true;
+                else if (left[i] < right[i])
+                    return false;
+            }
+
+            if (left.Length > right.Length)
+                return true;
+            else
+                return false;
+        }
+        public static StackPanel GetStack(Table table, Brush color)
+        {
+            var st = new StackPanel();
+            st.Orientation = Orientation.Horizontal;
+            st.Background = color;
+            st.Margin = new System.Windows.Thickness(5, 5, 5, 5);
+            foreach (var col in table.Column)
+            {
+                st.Children.Add(new TextBlock() { Text = col + ";" });
+            }
+            return st;
+        }
+    }
+}
